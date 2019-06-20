b35e38477ebfe4e96ae03cfe2226857c50346002
Nikolay Makhotkin <nikolay.makhotkin@gmail.com>
2019-06-20 19:07:51 +0400

    Try color correction; disable preserve paper

--- demo.py
+++ demo.py
@@ -64,6 +64,58 @@ def parse_args():
     return ap.parse_args()
 
 
+def apply_mask(matrix, mask, fill_value):
+    masked = np.ma.array(matrix, mask=mask, fill_value=fill_value)
+    return masked.filled()
+
+
+def apply_threshold(matrix, low_value, high_value):
+    low_mask = matrix < low_value
+    matrix = apply_mask(matrix, low_mask, low_value)
+
+    high_mask = matrix > high_value
+    matrix = apply_mask(matrix, high_mask, high_value)
+
+    return matrix
+
+
+def simplest_cb(img, percent):
+    assert img.shape[2] == 3
+    assert percent > 0 and percent < 100
+
+    half_percent = percent / 200.0
+
+    channels = cv2.split(img)
+
+    out_channels = []
+    for channel in channels:
+        assert len(channel.shape) == 2
+        # find the low and high precentile values (based on the input percentile)
+        height, width = channel.shape
+        vec_size = width * height
+        flat = channel.reshape(vec_size)
+
+        assert len(flat.shape) == 1
+
+        flat = np.sort(flat)
+
+        n_cols = flat.shape[0]
+
+        low_val = flat[int(np.floor(n_cols * half_percent))]
+        high_val = flat[int(np.ceil(n_cols * (1.0 - half_percent)))]
+
+        # print "Lowval: ", low_val
+        # print "Highval: ", high_val
+
+        # saturate below the low percentile and above the high percentile
+        thresholded = apply_threshold(channel, low_val, high_val)
+        # scale the channel
+        normalized = cv2.normalize(thresholded, thresholded.copy(), 0, 255, cv2.NORM_MINMAX)
+        out_channels.append(normalized)
+
+    return cv2.merge(out_channels)
+
+
 def str2bool(v):
     if v.lower() in ('yes', 'true', 't', 'y', '1'):
         return True
@@ -204,7 +256,8 @@ def process_video(input, input2, output, scale_fps=1):
             if prepared_stats is None:
                 prepared_stats = color_transfer.prepare_for_transfer(frame2)
 
-            colored = color_transfer.color_transfer_prepared(prepared_stats, frame1)
+            colored = color_transfer.color_transfer_prepared(prepared_stats, frame1, preserve_paper=False)
+            # colored = simplest_cb(colored, 20)
             cv2.putText(colored, "Reconstructed", (10, 30), font, 1, (0, 0, 0), 2, lineType=cv2.LINE_AA)
             cv2.putText(colored, "Reconstructed", (10, 30), font, 1, (200, 200, 200), 1, lineType=cv2.LINE_AA)
 
